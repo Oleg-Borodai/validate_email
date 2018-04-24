e5f47b3b904d038b593c8f8557daf3b42bf548d8
Oleg Borodai <oleg@borodai.com>
2018-04-24 18:39:38 +0200

    v1.2

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from distutils.core import setup
 setup(
   name = 'validemail',
   py_modules=('validemail',),
-  version = '1.1',
+  install_requires=['dnspython'],
+  version = '1.2',
   description = 'alidemail verifies if an email address is valid and really exists.',
   author = 'Oleg Borodai',
   author_email = 'oleg@borodai.com',
   url = 'https://github.com/oleg-borodai/validate_email',
-  download_url = 'https://github.com/oleg-borodai/validate_email/archive/1.1.tar.gz',
+  download_url = 'https://github.com/oleg-borodai/validate_email/archive/1.2.tar.gz',
   keywords = ['email', 'validation', 'dnspython'],
   classifiers = [],
 )
