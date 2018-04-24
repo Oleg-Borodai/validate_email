516ab408dcb0cd755e801992f8c067aa3ee8e4b8
Oleg Borodai <oleg@borodai.com>
2018-04-24 17:40:30 +0200

    adjust setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
-from setuptools import setup, find_packages
-
-setup(name='validemail',
-      version = '1.0',
-      download_url = 'git@github.com:oleg-borodai/validate_email.git',
-      py_modules = ('validemail',),
-      author = 'Oleg Borodai',
-      author_email = 'oleg@borodai.com',
-      description = 'validemail verifies if an email address is valid and really exists.',
-      long_description=open('README.rst').read(),
-      keywords = 'email validation verification mx verify',
-      url = 'https://github.com/oleg-borodai/validate_email',
-      license = 'LGPL',
-    )
+from distutils.core import setup
+setup(
+  name = 'validemail',
+  packages = ['validemail'],
+  version = '1.1',
+  description = 'alidemail verifies if an email address is valid and really exists.',
+  author = 'Oleg Borodai',
+  author_email = 'oleg@borodai.com',
+  url = 'https://github.com/oleg-borodai/validate_email',
+  download_url = 'https://github.com/oleg-borodai/validate_email/archive/1.1.tar.gz',
+  keywords = ['email', 'validation', 'dnspython'],
+  classifiers = [],
+)
